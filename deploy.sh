8d8e505d2e12082bec3960adbc7fd23c2271241e
wxingfu <wxffxw.664587@gmail.com>
2023-02-03 11:04:07 +0800

    20230203

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
-
 # 生成静态文件
 npm run docs:build
-
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m $(date "+%Y%m%d%H%M%S")
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
